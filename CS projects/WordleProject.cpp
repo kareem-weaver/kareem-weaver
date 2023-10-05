4b8d2fbcdf783aad516689fb2fc1f7640c8b4ebc
kareem-weaver <133832093+kareem-weaver@users.noreply.github.com>
2023-10-04 20:02:26 -0400

    Update WordleProject.cpp

--- CS projects/WordleProject.cpp
+++ CS projects/WordleProject.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std; 
 int main () {
     int guessIndex = 0;
@@ -7,17 +8,22 @@ int main () {
     string guess;
     int guessNum = 0;
         
+    string red = "\033[1;31m";
+    string green = "\033[1;32m";
+    string yellow = "\033[1;33m";
+    string reset = "\033[0m";
+
         cout <<"Guess a five letter word: ";
         cin >>guess;
 
     while (guess != answer && guessNum <= 5) {
         while (guessIndex <= 4 && guessNum <= 5) {
             if (guess[guessIndex] == answer[answerIndex]) {
-                cout << guess[guessIndex] <<" is green\n";
+                cout << green << guess[guessIndex] << reset << endl; //green
             } else if ((guess[guessIndex] != answer[answerIndex]) && (guess[guessIndex] == answer[0] || guess[guessIndex] == answer[1] || guess[guessIndex] == answer[2] || guess[guessIndex] == answer[3] || guess[guessIndex] == answer[4])) {      
-                cout << guess[guessIndex] <<" is yellow\n";
+                cout << yellow << guess[guessIndex] << reset << endl; //yellow
             } else if (guess[guessIndex] != answer[0] || guess[guessIndex] != answer[1] || guess[guessIndex] != answer[2] || guess[guessIndex] != answer[3] || guess[guessIndex] != answer[4]) {
-                cout << guess[guessIndex] <<" is red" << endl;
+                cout << red << guess[guessIndex] << reset << endl; //red
             }
             guessIndex = guessIndex + 1;
             answerIndex = answerIndex + 1;
@@ -30,10 +36,9 @@ int main () {
             answerIndex = 0;
         }
     } 
-    
     if (guessNum = 6 && guess != answer) {
         cout <<"You are out of guesses. The Answer was HORSE \n"; 
     } else if (guess == answer)  
-    cout << "You guessed the word, " << answer <<endl;
+    cout << "You guessed the word: " << green << answer << reset << "!" << endl;
     return 0;
 }
